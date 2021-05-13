4a02ff1584d018bc8a5659663cf8ccdf77267730
Zaur <fameless.4ell@gmail.com>
2021-05-13 23:37:22 +0200

    Добавьте файлы проекта.

---
+++ WebTODOapp/Areas/Identity/Data/WebTODOappUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Castle.MicroKernel.SubSystems.Conversion;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebTODOapp.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the WebTODOappUser class
+    public class WebTODOappUser : IdentityUser
+    {
+        [PersonalData]
+        public string Firstname { get; set; }
+        [PersonalData]
+        public string LastName { get; set; }
+    }
+}
---
+++ WebTODOapp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebTODOapp.Areas.Identity.Data;
+using WebTODOapp.Data;
+
+[assembly: HostingStartup(typeof(WebTODOapp.Areas.Identity.IdentityHostingStartup))]
+namespace WebTODOapp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<OAuthContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("OAuthContextConnection")));
+
+                services.AddDefaultIdentity<WebTODOappUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<OAuthContext>();
+            });
+        }
+    }
+}
